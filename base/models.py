d7490455a8dc5fd68e054afebb7136560101d4a8
RubelBiswasCS <rubelbiswas@gmail.com>
2021-12-10 20:36:54 +0600

    added product model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Color(models.Model):
+    class Meta:
+        verbose_name = 'Color'
+        verbose_name_plural = 'Colors'
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+    
+    name = models.CharField(max_length=100)
+    model = models.CharField(null=True,blank=True,max_length=50)
+    brand = models.CharField(null=True,blank=True,max_length=50)
+    color = models.ForeignKey(Category,on_delete=models.PROTECT,default=1)
+    unit_price = models.FloatField()
+    current_stock = models.IntegerField(default=10,null=True,blank=True)
+    image = models.FileField(blank=True,null=True,upload_to='products')
+    details = models.TextField()
+
+    def __str__(self):
+        return self.name
