dfae585e6f546f6ba9257686c56c65a86b519738
RubelBiswasCS <rubelbiswas36@gmail.com>
2021-12-23 13:29:35 +0600

    modified settings

--- core/settings.py
+++ core/settings.py
@@ -147,7 +147,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://ch-store.netlify.app"
+    "https://ch-store.netlify.app",
 ]
 
 
