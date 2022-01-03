4021b919016be0eb0cd669d8a3593a500dec3d72
RubelBiswasCS <rubelbiswas36@gmail.com>
2022-01-03 22:33:15 +0600

    added custom user model

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import users
+from .models import CustomUser
 
-admin.site.register(users)
+admin.site.register(CustomUser)
--- users/models.py
+++ users/models.py
@@ -5,3 +5,47 @@ from django.contrib.auth.models import (
                             PermissionsMixin
                             )
 
+class CustomUserManager(BaseUserManager):
+    def create_superuser(self,email,name,password, **other_fields):
+        other_fields.setdefault('is_staff',True)
+        other_fields.setdefault('is_superuser',True)
+        other_fields.setdefault('is_active',True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_superuser=True')
+        return self.create_user(email,name,password,**other_fields)
+    
+    def create_user(self,email, name, password, **other_fields):
+        if not email:
+            raise ValueError('You must provide an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email = email, name = name, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=150)
+    phone = models.CharField(max_length=20,blank=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.name
