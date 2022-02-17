e4f210f5dbf54b0a6cac0e0dec301b0b081df998
RubelBiswasCS <rubelbiswas36@gmail.com>
2022-02-18 01:32:23 +0600

    modified users views, serializers, core settings and api views

--- users/serializers.py
+++ users/serializers.py
@@ -10,12 +10,12 @@ class CustomUserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ['email','name','password']
 
-    def create_user(self, validated_data):
+    def create(self, validated_data):
         password = validated_data.pop('password',None)
         instance = self.Meta.model(**validated_data)
 
         if password is not None:
-            isinstance.set_password(password)
+            instance.set_password(password)
         instance.save()
 
         return instance
--- users/views.py
+++ users/views.py
@@ -9,10 +9,11 @@ class CustomUserCreate(APIView):
     
     def post(self, request, format='json'):
         serializer = CustomUserSerializer(data=request.data)
+        print('data:',request.data)
         if serializer.is_valid():
             user = serializer.save()
             if user:
                 json = serializer.data
-                return Response(json, status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
